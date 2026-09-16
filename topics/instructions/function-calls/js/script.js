26f143a170e8d9fa5c8ed0097b9fee450c69e09d
Matteo Edmonds-Tiano <tianomatt21@gmail.com>
2026-09-16 13:54:34 -0400

    Finished simulation of write with writer's block (LOL)!

--- topics/instructions/function-calls/js/script.js
+++ topics/instructions/function-calls/js/script.js
@@ -1,24 +1,24 @@
 /**
- * Title of Project
- * Author Name
+ * Function Calls
+ * Matteo Edmonds-Tiano
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Working on the function calls examples.
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
 
+function setup() {
+    // Once at the beginning of the program
+    createCanvas(640,480);
 }
 
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
+    // Every frame(essentially as long as its up you'll see it!)
+    background(255, 100, 100);
+    rect(200, 80, 240, 320); 
+    // First number is the x, Second is the y, and the 3rd and 4th are the width and height
+    
 
 }
