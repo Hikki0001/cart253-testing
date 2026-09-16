baaa5539fea7d423456cad98041e8ec0619deb3c
Matteo Edmonds-Tiano <tianomatt21@gmail.com>
2026-09-16 14:04:58 -0400

    Added comments to the blank page project

--- topics/instructions/function-calls/js/script.js
+++ topics/instructions/function-calls/js/script.js
@@ -1,24 +1,35 @@
 /**
- * Function Calls
+ * The Blank Page
  * Matteo Edmonds-Tiano
  * 
- * Working on the function calls examples.
+ * An exploration of the existential angst of a novelist 
+ * who must sit down at their pink desk and confront the
+ * abyss that is a blank page.
+ * 
+ * The program is a non-interactive to convey the inability 
+ * to get started on the project. Try typing! JUST TRY IT!!!!!
  */
 
 "use strict";
 
-
+/** 
+ * Creates the Canvas for our masterpiece
+*/
 function setup() {
-    // Once at the beginning of the program
+    // Create a canvas at a standard resolution
     createCanvas(640,480);
 }
 
-
+/**
+ * Draws the writes's desktop and a blank piece of paper
+ */
 function draw() {
-    // Every frame(essentially as long as its up you'll see it!)
+    // The pink desktop
+    // Every frame(essentially as long as its up you'll see it!)(keeping this for myself)
     background(255, 100, 100);
+    // The blank piece of paper(Draws a rectangle)
     rect(200, 80, 240, 320); 
     // First number is the x, Second is the y, and the 3rd and 4th are the width and height
-    
+
 
 }
