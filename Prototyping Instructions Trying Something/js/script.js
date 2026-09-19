f0a7c05f24bd502b31b08ea4df7ec65a46d0a6b6
Matteo Edmonds-Tiano <tianomatt21@gmail.com>
2026-09-18 22:54:29 -0400

    starting Project and adding stairs

---
+++ Prototyping Instructions Trying Something/js/script.js
@@ -0,0 +1,78 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
+ * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(640, 640)
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+    background(255, 0, 0)
+
+    drawStairs();
+
+}
+
+function drawStairs() {
+    push();
+    noStroke();
+    fill(0);
+    rect(200, 200, 100, 50);
+    pop();
+
+    push();
+    noStroke();
+    fill(0);
+    rect(200, 210, 200, 50);
+    pop();
+
+    push();
+    noStroke();
+    fill(0);
+    rect(100, 240, 300, 50);
+    pop();
+
+    push();
+    noStroke();
+    fill(0);
+    rect(220, 280, 300, 50);
+    pop();
+
+    push();
+    noStroke();
+    fill(0);
+    rect(150, 310, 300, 50);
+    pop();
+
+    push();
+    noStroke();
+    fill(0);
+    rect(80, 350, 160, 50);
+    pop();
+
+    push();
+    noStroke();
+    fill(0);
+    rect(200, 380, 200, 50);
+    pop();
+
+    push();
+    noStroke();
+    fill(0);
+    rect(200, 380, 200, 50);
+    pop();
+}
