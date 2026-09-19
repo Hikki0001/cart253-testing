30a907a750239649463ee97b0b4d3b47eb2cf57b
Matteo Edmonds-Tiano <tianomatt21@gmail.com>
2026-09-18 21:36:53 -0400

    Added Kirby's body parts and the Moon

--- Prototyping Instructions Kirby/js/script.js
+++ Prototyping Instructions Kirby/js/script.js
@@ -26,7 +26,8 @@ function draw() {
 
     drawSky();
     drawGrass();
-
+    drawMoon();
+    drawKirby1();
 }
 
 function drawSky() {
@@ -60,5 +61,142 @@ function drawGrass() {
     ellipse(150, 600, 600, 300)
     rect(-150, 600, 600, 150);
     pop();
+}
+
+function drawMoon() {
+    push();
+    noStroke();
+    fill(110, 127, 128)
+    ellipse(550, 100, 100, 100)
+    pop();
+
+    /**
+     * Small Crater
+     */
+    push();
+    noStroke();
+    fill(54, 69, 79);
+    ellipse(565, 120, 25, 25);
+    pop();
+
+    /**
+     * Bigger Crater
+     */
+    push();
+    noStroke();
+    fill(54, 69, 79);
+    ellipse(535, 90, 40, 40)
+    pop();
+}
+
+function drawKirby1() {
+
+    /**
+         * Kirby's left arm
+         */
+
+    push();
+    noStroke();
+    fill(249, 136, 190);
+    ellipse(140, 480, 120, 200)
+    pop();
+
+    /**
+    * Kirby's right arm
+    */
+    push();
+    noStroke();
+    fill(249, 136, 190);
+    ellipse(500, 480, 120, 200)
+    pop();
+
+
+    /**
+     * His left leg
+     */
+    push();
+    noStroke();
+    fill(236, 85, 104);
+    ellipse(240, 590, 150, 250)
+    pop();
+
+    /**
+    * His right leg
+    */
+    push();
+    noStroke();
+    fill(236, 85, 104);
+    ellipse(420, 590, 150, 250)
+    pop();
+
+
+
+
+    /**
+     * His Base Body
+     */
+    push();
+    noStroke();
+    fill(251, 181, 207);
+    ellipse(320, 420, 400, 400)
+    pop();
+
+    /**
+     * The black parts of his eyes
+     */
+    push();
+    noStroke();
+    fill(0, 0, 0);
+    ellipse(265, 370, 60, 150)
+    pop();
+
+    /**
+     * The black part of his eyes right side
+     */
+    push();
+    noStroke();
+    fill(0, 0, 0);
+    ellipse(375, 370, 60, 150)
+    pop();
+
+    /**
+     * the White part of his eyes
+     */
+    push();
+    noStroke();
+    fill(255, 255, 255);
+    ellipse(265, 340, 40, 80)
+    pop();
+
+    /**
+     * the white part of his eyes right side
+     */
+    push();
+    noStroke();
+    fill(255, 255, 255);
+    ellipse(375, 340, 40, 80)
+    pop();
+
+    /**
+     * Kirby's cheeks
+     */
+
+    push();
+    noStroke();
+    fill(249, 136, 190)
+    ellipse(215, 480, 80, 60)
+    pop();
+
+    /**
+     * Right cheek
+     */
+    push();
+    noStroke();
+    fill(249, 136, 190)
+    ellipse(430, 480, 80, 60)
+    pop();
+
+
 
 }
+
