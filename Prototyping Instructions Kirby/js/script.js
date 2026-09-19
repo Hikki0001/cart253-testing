8ac4a81aacaf0bfd41b95ed477d6a94114843eea
Matteo Edmonds-Tiano <tianomatt21@gmail.com>
2026-09-18 20:47:11 -0400

    Added a grass function to the code and to the drawing

--- Prototyping Instructions Kirby/js/script.js
+++ Prototyping Instructions Kirby/js/script.js
@@ -25,6 +25,7 @@ function draw() {
     background(173, 216, 240)
 
     drawSky();
+    drawGrass();
 
 }
 
@@ -41,6 +42,23 @@ function drawSky() {
     colorMode(HSL, 100);
     fill(53, 50, 70);
     rect(0, 0, 640, 200);
+    pop();
+}
+
+function drawGrass() {
+    push();
+    noStroke();
+    colorMode(HSL, 100)
+    fill(25, 240, 31)
+    ellipse(450, 600, 600, 300)
+    pop();
+
+    push();
+    noStroke();
+    colorMode(HSL, 100)
+    fill(36, 174, 36)
+    ellipse(150, 600, 600, 300)
+    rect(-150, 600, 600, 150);
     pop();
 
 }
